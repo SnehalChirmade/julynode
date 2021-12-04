d5559eb306b830d2a4c612eb3b12e049ba309005
SnehalChirmade <snehal1061993@gmail.com>
2021-12-04 14:13:08 +0530

    added menu

--- app.js
+++ app.js
@@ -101,6 +101,15 @@ app.get('/details/:id',(req,res) => {
     })
 })
 
+//menu Details
+app.get('/menu/:id',(req,res) => {
+    var id = req.params.id
+    db.collection('menu').find({restaurant_id:Number(id)}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
 // place order 
 app.post('/placeOrder',(req,res) => {
     console.log(req.body);
