e679d2b009db2d9ad95c18fcebc30284f9c90877
SnehalChirmade <snehal1061993@gmail.com>
2021-12-07 23:28:15 +0530

    code

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.get('/menu/:id', (req, res) => {
 })
 
 app.post('/menuItem', (req, res) => {
-    db.collection('menu').find({ menu_id: { $in: req.body.ids } }).toArray((err, result) => {
+    db.collection('menu').find({menu_id:{$in:req.body}}).toArray((err, result) => {
         if (err) throw err;
         res.send(result)
     })
